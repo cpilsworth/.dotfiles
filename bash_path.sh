07328d4a752dfd593183b831f76d6e1ef3bdc2a1
Chris Pilsworth <cpilsworth@gmail.com>
2015-09-17 16:41:38 +0100

    Removed postgresql path

--- bash_path.sh
+++ bash_path.sh
@@ -2,7 +2,5 @@
 [[ -d /usr/local/bin ]] && export PATH="/usr/local/bin:$PATH"
 [[ -d /opt/local/sbin ]] && export PATH="/opt/local/sbin:$PATH"
 [[ -d /opt/local/bin ]] && export PATH="/opt/local/bin:$PATH"
-[[ -d /opt/local/lib/postgresql83/bin ]] && export PATH="/opt/local/lib/postgresql83/bin:$PATH"
 [[ -d $DF_BASE_PATH/bin ]] && export PATH="$DF_BASE_PATH/bin:$PATH"
 [[ -d $HOME/bin ]] && export PATH="$HOME/bin:$PATH"
-
