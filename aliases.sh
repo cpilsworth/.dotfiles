1dc8e05b201b084d5dba6ca48c265e1d550648bc
Chris Pilsworth <cpilsworth@gmail.com>
2015-10-16 16:17:32 +0100

    Added a command to lock screen

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,5 @@
 alias ll="ls -la"
 
 alias findpid="ps axww -o pid,user,%cpu,%mem,start,time,command | selecta | sed 's/^ *//' | cut -f1 -d' '"
+
+alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
