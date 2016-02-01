2f6b8416cc6fcd92adac36c7911cbf6937170fae
Chris Pilsworth <cpilsworth@gmail.com>
2016-02-01 13:44:05 +0000

    Added some terraform aliases

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,11 @@ alias ll="ls -la"
 alias findpid="ps axww -o pid,user,%cpu,%mem,start,time,command | selecta | sed 's/^ *//' | cut -f1 -d' '"
 
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
+alias mcia="mvn clean install -PautoInstallPackage"
+alias mcip="mvn clean install -PautoInstallPackagePublish"
+
+alias tf="terraform"
+alias tfp="terraform plan"
+alias tfa="terraform apply" 
+alias tfd="terraform destroy" 
